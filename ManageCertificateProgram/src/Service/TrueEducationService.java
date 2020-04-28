abb9bdd5cc0a63d2d5300b9abae0b987bc0f051a
themiyaweerakoon <59858572+themiyaweerakoon@users.noreply.github.com>
2020-04-29 02:46:02 +0530

    Update

--- ManageCertificateProgram/src/Service/TrueEducationService.java
+++ ManageCertificateProgram/src/Service/TrueEducationService.java
@@ -13,4 +13,16 @@ public void addStudent(Student student)
     DB.addStudent(student);
 }
 
+public void deleteStudent(Student student)
+{
+    Database DB = new Database();
+    DB.deleteStudent(student);
+}
+
+public void viewStudent(Student student)
+{
+    Database DB = new Database();
+    DB.viewStudent(student);
+}
+
 }
