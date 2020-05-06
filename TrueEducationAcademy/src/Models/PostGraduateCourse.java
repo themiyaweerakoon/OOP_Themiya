41e41af57d296eefc51ba4309ec325461934455e
RadhishaDeMel <63054709+RadhishaDeMel@users.noreply.github.com>
2020-05-07 00:29:49 +0530

    Final Commit IT18256024
    J.R De Mel
    Manage postgraduate degree programs

---
+++ TrueEducationAcademy/src/Models/PostGraduateCourse.java
@@ -0,0 +1,29 @@
+
+package Models;
+
+/**
+ *
+ * @author Jeremy
+ */
+public class PostGraduateCourse {
+        private String ModuleID;
+        private String ModuleName;
+
+    public void setModuleID(String ModuleID) {
+        this.ModuleID = ModuleID;
+    }
+
+    public void setModuleName(String ModuleName) {
+        this.ModuleName = ModuleName;
+    }
+
+    public String getModuleID() {
+        return ModuleID;
+    }
+
+    public String getModuleName() {
+        return ModuleName;
+    }
+
+    
+}
